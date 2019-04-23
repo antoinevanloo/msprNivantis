71d33ef37cb714a119ca2c25fbca5e3b4999124f
Azgarion <arthur.td@hotmail.fr>
2019-04-23 10:52:37 +0200

    base config

---
+++ src/main/java/nivantis/MaServlet.java
@@ -0,0 +1,49 @@
+package nivantis;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MaServlet
+ */
+@WebServlet("/test")
+public class MaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@PersistenceContext(name="nivantis")
+	private EntityManager entityManager;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MaServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		BigInteger resultat = (BigInteger) entityManager.createNativeQuery("select 1").getSingleResult();
+		response.getWriter().append("Résultat depuis la base de données : ").append(String.valueOf(resultat));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
